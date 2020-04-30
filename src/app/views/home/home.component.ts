94071118fe78d7f9ceec2d89634ae2d8cd371e85
Rodrigo Silva <jrodrigo887@gmail.com>
2020-04-29 21:35:21 -0300

    Adição de mais um chart na home

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderService } from 'src/app/componentes/template/header/header.service';
-import { PatternValidator } from '@angular/forms';
-
+import { ChartComponent } from 'angular2-chartjs'
 
 @Component({
  selector: 'app-home',
@@ -49,7 +48,46 @@ options = {
   maintainAspectRatio: false
 };
 
+
+
+type2 = 'bar';
+data2 = {
+    labels: ['Criança', 'Pre-Adolescente', 'Adolescente', 'Jovem', 'Adulto', 'Idoso'],
+    datasets: [{
+        label: 'Resultados',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+    }]
+};
+options2 = {
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero: true
+            }
+        }]
+    }
+};
+
+
  ngOnInit(): void {
+
  }
 
 }
